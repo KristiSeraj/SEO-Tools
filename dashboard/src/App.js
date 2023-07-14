27a9cc2205a2235cfd7e6ac173c130a5b4b04ee7
KristiSeraj <serajkristi@gmail.com>
2023-07-14 14:54:48 +0200

    Header component added

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import Keyword from './Keyword/Keyword';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Keyword />
     </div>
   );
---
+++ dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <nav className='nav'>
+            <h2>SEO Tools</h2>
+        </nav>
+    );
+}
+ 
+export default Header;
